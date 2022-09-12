88bc2e97ba876020b9449c18f461f6100d2eb244
Sudarshan Kumar Kumar <97657677+Technetron512@users.noreply.github.com>
2022-09-12 05:50:08 +0000

    operations on C++ stl array

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
-
+#include<array>
+using namespace std;
 int main() {
-    std::cout << "Hello, world!";
-    return 0;
+  array<int,4> a= {1,2,4,5};
+  cout<<"Size of array a is : "<<a.size()<<endl;
+  for(int i = 0 ; i < a.size() ; i++){
+      cout<<a[i]<<endl;
+  }
+  cout<<"EMpty or not "<<a.empty()<<endl;
+  cout<<"ELement at position " <<a.at(2)<<endl;
+  cout<<"The first element is : "<<a.front()<<endl;
+  cout<<"The last element is : "<<a.back()<<endl;
+  return 0;
 }
